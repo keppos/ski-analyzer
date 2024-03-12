43906310ebaed811cbe184fa75912565139c9e9f
keppos <kevin.osterberg@hotmail.com>
2024-03-12 23:44:31 +0200

    Final commit

--- predict.py
+++ predict.py
@@ -5,4 +5,4 @@ from ultralytics import YOLO
 model = YOLO("D:/Users/Kevin/VSC Projects/ski-analyzer/runs/detect/train11/weights/last.pt")
 
 # Load image
-model.predict("D:/Users/Kevin/VSC Projects/ski-analyzer/data/images/test/testimage1.png", save=True)
+model.predict("D:/Users/Kevin/VSC Projects/ski-analyzer/data/images/test/testimage2.png", save=True)
